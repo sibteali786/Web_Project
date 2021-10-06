47e225d83404a9500be60af50c7f6996e86c5f19
Syed_Sibteali_Baqar <ssibteali.ce41ceme@ce.ceme.edu.pk>
2021-10-06 08:57:12 +0500

    done  with landing page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,30 +6,43 @@ const Hero = () => {
     <Fragment>
       <section style={{ marginTop: "80px" }} className={`bg-dark p-5`}>
         <div className="container">
-          <div className="d-flex justify-content-start align-content-center">
-            <button
-              className={`${styles.btnText} btn btn-outline-light mx-2 my-4 flex-grow-1`}
-            >
-              <i className="fa fa-graduation-cap mx-2"></i> Careers
-            </button>
-            <button
-              className={`${styles.btnText} btn btn-primary mx-2 my-4 flex-grow-1`}
-            >
-              <i className="fa fa-sign-in mx-2"></i> Register
-            </button>
-          </div>
-          <div className="d-block d-md-flex align-content-md-center md-justify-content-md-between mt-5 ">
-            <div className="w-50">
-              <p className="fw-bold h3 text-light">
-                Empowering youth through capacity building{" "}
-              </p>
-              <p className="fs-6 text-light " style={{ opacity: "0.8" }}>
-                GuleShaoor nurtures youth and maximizes its potential by
-                providing mentorship, counselling and networks for their
-                personal as well as career development.
-              </p>
+          <div className="d-grid">
+            <div className="row">
+              <div className="col-sm-12 col-lg-5 d-flex flex-column ">
+                <div className="d-flex justify-content-start align-content-center">
+                  <button
+                    className={`${styles.btnText} btn btn-outline-light mx-2 my-4 flex-grow-1`}
+                  >
+                    <i className="fa fa-graduation-cap mx-2"></i> Careers
+                  </button>
+                  <button
+                    className={`${styles.btnText} btn btn-primary mx-2 my-4 flex-grow-1`}
+                  >
+                    <i className="fa fa-sign-in mx-2"></i> Register
+                  </button>
+                </div>
+                <div className="py-3">
+                  <p className="fw-bold h2 text-light">
+                    Empowering youth through capacity building{" "}
+                  </p>
+                  <p
+                    className="fs-6 text-light py-3 "
+                    style={{ opacity: "0.8" }}
+                  >
+                    GuleShaoor nurtures youth and maximizes its potential by
+                    providing mentorship, counselling and networks for their
+                    personal as well as career development.
+                  </p>
+                </div>
+              </div>
+              <div className="col-sm-12 col-lg-7">
+                <img
+                  src={require("../Images/GuleShaoorWord.svg").default}
+                  className="img-fluid"
+                  alt=""
+                />
+              </div>
             </div>
-            <div>{/* Gule Shaoor Image */}</div>
           </div>
         </div>
       </section>
