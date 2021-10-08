0a3ddcc21958dd25f4ebc5a4368d63c45e244982
Syed_Sibteali_Baqar <ssibteali.ce41ceme@ce.ceme.edu.pk>
2021-10-08 09:53:18 +0500

    rearranged things

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Services from "../Pages/Services";
-import Mentorship from "../Pages/Mentorship";
-import Header from "../components/mentorship-section/Header";
+import Header from "../components/Header";
 import Mentors from "../components/Mentors";
 import CareerCounsellors from "../components/CareerCounsellors";
-import Auth from "../Pages/Auth";
 const Home = () => {
   return (
     <div>
